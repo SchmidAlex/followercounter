b8944405aecdf07cac4c5517e8f2a44d5afc7f39
quibiq-cmo <59870186+quibiq-cmo@users.noreply.github.com>
2021-12-03 15:01:44 +0100

    generate db.json success

--- functions.js
+++ functions.js
@@ -5,34 +5,33 @@ const Insta = require('scraper-instagram');
 const InstaClient = new Insta();
 
 const fetchFollowers = function (element) {
-    return new Promise(function (resolve, reject) {
+    return new Promise(resolve => {
         //if element.account is empty
-        switch (element.name) {
-            case 'twitter':
-                const consumerClient = new TwitterApi({
-                    appKey: 'YmKQuTbgly0XmoCFfg8UUp5xc',
-                    appSecret:
-                        'R2QwdoMbG4Qx3WwWy8C4bpl0NnfYQ9IvvaPKyM8YG8tQUZd8yI',
-                });
-                consumerClient.appLogin().then((client) => {
-                    client.v1.searchUsers(element.account).then((user) => {
-                        element.follower = user._realData[0].followers_count;
-                        console.log('this should be the first console log');
-                        console.log(element.follower);
-                        resolve(element);
+        
+            console.log(element);
+            switch (element.name) {
+                case 'twitter':
+                    const consumerClient = new TwitterApi({
+                        appKey: 'YmKQuTbgly0XmoCFfg8UUp5xc',
+                        appSecret:
+                            'R2QwdoMbG4Qx3WwWy8C4bpl0NnfYQ9IvvaPKyM8YG8tQUZd8yI',
                     });
-                });
-                break;
-            case 'instagram':
-                InstaClient.getProfile(element.account).then((profile) => {
-                    element.follower = profile.followers;
-                    resolve(element);
-                });
-                break;
-            default:
-                reject('fail');
-                break;
-        }
+                    consumerClient.appLogin().then((client) => {
+                        client.v1.searchUsers(element.account).then((user) => {
+                            element.follower = user._realData[0].followers_count;
+                            resolve(element);
+                        }).catch((err) => console.log("Error!", err)); 
+                    });
+                    break;
+                case 'instagram':
+                    InstaClient.getProfile(element.account).then((profile) => {
+                        element.follower = profile.followers;
+                        resolve(element);
+                    }).catch((err) =>  console.log("Error!", err)); 
+                    break;
+            }
+        
+ 
     });
 };
 
