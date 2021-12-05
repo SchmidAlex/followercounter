69e24a16f7f2de135770c70a2afe303081f9dc07
defkon98 <alex_schmid@hotmail.ch>
2021-12-05 14:17:53 +0100

    testing

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ ipcMain.on('get-followers', (event) => {
             accounts = values; // every promise is replaced by its element
             console.log('Accounts', accounts);
             fs.writeFileSync(configFile, JSON.stringify(accounts));
-            event.reply('get-followers-reply');
+            event.reply('get-followers-reply', accounts); //why does this reply not work??
         })
         .catch((err) => console.log('Error!', err));
 });
